ab554c7ab3bb512644a08ead9029becbf5281165
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-02-17 19:55:32 +0100

    add a dash of css

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,13 +4,17 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <title>About</title>
     </head>
     <body>
+        
         <h1>Blog</h1>
         @foreach ($posts as $item)
+        <div>
         <h2>{{ $item -> title  }}</h2>
         <p>{{ $item -> body  }}</p>
+        </div>
         
             
         @endforeach
